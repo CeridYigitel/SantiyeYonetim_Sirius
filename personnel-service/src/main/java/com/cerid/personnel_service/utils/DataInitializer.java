b1508906183be24650bb322fe968503421733859
CeridYigitel <cerityigitel@hotmail.com>
2026-06-09 22:43:21 +0300

    duzeltmeler

--- personnel-service/src/main/java/com/cerid/personnel_service/utils/DataInitializer.java
+++ personnel-service/src/main/java/com/cerid/personnel_service/utils/DataInitializer.java
@@ -1,9 +1,6 @@
 package com.cerid.personnel_service.utils;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -26,33 +23,11 @@ public class DataInitializer implements CommandLineRunner {
             
             // 1. Admin Ekle
             User admin = User.builder()
-                    .username("admin")
-                    .passwordHash("1234") // Gerçekte BCrypt olacak
+                    .username("Hasan Berat Şahin")
+                    .passwordHash("Shn9Hsn10Brt15") // Gerçekte BCrypt olacak
                     .role(Role.ADMIN).hasPurchasingAuthority(true).hourlyWage(2000d)
                     .build();
             userRepository.save(admin);
-
-            // 2. Usta Ekle
-            User usta = User.builder()
-                    .username("usta_ahmet")
-                    .passwordHash("1234")
-                    .role(Role.FOREMAN).hasPurchasingAuthority(false).hourlyWage(1000d)
-                    .build();
-            userRepository.save(usta);
-
-            List<User> ustalar = new ArrayList<>();
-            ustalar.add(usta);
-
-
-
-            // 3. İşçi Ekle (Ustası "usta_ahmet" olacak şekilde)
-            User isci = User.builder()
-                    .username("isci_mehmet")
-                    .passwordHash("1234")
-                    .role(Role.WORKER).hasPurchasingAuthority(false).hourlyWage(1000d)
-                    .supervisors(ustalar) // Hiyerarşiyi burada kuruyoruz!
-                    .build();
-            userRepository.save(isci);
 
             System.out.println("Test verileri (Admin, Usta, İşçi) başarıyla oluşturuldu!");
         }
